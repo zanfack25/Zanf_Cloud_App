ab3b7b05f9efa56959781b5410760a1bd95f6515
zanfack25 <zanfackdave@gmail.com>
2025-12-01 14:13:39 -0500

    Add hostname and IP address routes
    Added routes to return hostname and IP address.

--- app.py
+++ app.py
@@ -2,7 +2,16 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+hostname = socket.gethostname()
+ip_address = socket.gethostbyname(hostname)
+
+@app.route('/host')
+def host_name():
+  return hostname
+    
+@app.route('/ip')
+def host_ip():
+  return ip_address
 
 @app.route('/')
 def hello_cloud():
@@ -12,6 +21,7 @@ def hello_cloud():
 def health():
     # Return a simple 200 OK response
     return 'OK', 200
+app = Flask(__name__)
 
 if __name__ == '__main__':
     # Keep port consistent with ECS/ALB config
