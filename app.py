8123d8382f86026ce3e91df8306389d6f89badbf
zanfack25 <zanfackdave@gmail.com>
2025-12-01 21:11:58 -0500

    Clean up app.py by removing socket functionality
    Removed unused socket code and related routes.

--- app.py
+++ app.py
@@ -1,20 +1,9 @@
 from flask import Flask
 from flask_cors import CORS
-import socket
 
 app = Flask(__name__)
 CORS(app)  # enable CORS if needed
 
-hostname = socket.gethostname()
-ip_address = socket.gethostbyname(hostname)
-
-@app.route('/host')
-def host_name():
-    return hostname
-
-@app.route('/ip')
-def host_ip():
-    return ip_address
 
 @app.route('/')
 def hello_cloud():
